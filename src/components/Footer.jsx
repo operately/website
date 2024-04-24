3c3dbb9dfef3aec91c980fa223b30a5424e74568
Marko Anastasov <marko@operately.com>
2024-04-24 14:34:45 +0200

    Color here and there.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,7 +45,7 @@ const navigation = {
           <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
             {navigation.main.map((item) => (
               <div key={item.name} className="pb-6">
-                <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+                <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-operately-blue">
                   {item.name}
                 </a>
               </div>
@@ -53,7 +53,7 @@ const navigation = {
           </nav>
           <div className="mt-10 flex justify-center space-x-10">
             {navigation.social.map((item) => (
-              <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+              <a key={item.name} href={item.href} className="text-gray-400 hover:text-operately-blue">
                 <span className="sr-only">{item.name}</span>
                 <item.icon className="h-6 w-6" aria-hidden="true" />
               </a>
