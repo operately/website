e5f1a12d929785c43a4354733b175c3c99c793bf
Marko Anastasov <marko@operately.com>
2024-05-24 18:16:31 +0200

    Handbook: anchor links for headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,32 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://operately.com",
-  integrations: [tailwind(), react(), mdx()]
+  integrations: [
+    tailwind(),
+    react(),
+    mdx({
+      remarkPlugins: [],
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutolinkHeadings, {
+          behavior: 'append',
+          properties: {
+            className: ['anchor-link']
+          },
+          content: [{
+            type: 'element',
+            tagName: 'span',
+            properties: {className: ['hash-symbol']},
+            children: [{type: 'text', value: '#'}]
+          }]
+        }]
+      ]
+    })
+  ]
 });
