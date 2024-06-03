629423f2433e108f50e19d0d47441b5eecee829f
Igor Šarčević <igor@operately.com>
2024-06-03 11:51:42 +0000

    Make list titles smaller
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- src/components/SideBySideList.jsx
+++ src/components/SideBySideList.jsx
@@ -1,7 +1,7 @@
 export const SideBySideList = (props) => (
   <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
     <div>
-      <h2 className="font-bold">{props.left.title}</h2>
+      <div className="font-bold">{props.left.title}</div>
       <ul>
         {props.left.items.map((item, index) => (
           <li key={index}>{item}</li>
@@ -10,7 +10,7 @@ export const SideBySideList = (props) => (
     </div>
 
     <div>
-      <h2 className="font-bold">{props.right.title}</h2>
+      <div className="font-bold">{props.right.title}</div>
       <ul>
         {props.right.items.map((item, index) => (
           <li key={index}>{item}</li>
