ba3aab8b33dc448d192b8aedc43af8f2ff6a66ad
Marko Anastasov <marko@operately.com>
2024-09-03 16:18:05 +0200

    Add Releases to top navigation
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,8 @@ const githubUrl = import.meta.env.GITHUB_URL;
 const xUrl = import.meta.env.X_URL;
 
 const navigation = [
-  /*{ name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
+  { name: "Releases", href: "/releases" }
+  /*{ name: 'Features', href: '#' },
   { name: 'Company', href: '#' }*/
 ]
 
@@ -20,57 +19,56 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-      <header className="absolute inset-x-0 top-0 z-10">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 p-1.5">
-              <img className="h-6 w-auto" src={logoImage.src} alt="Operately logo" />
-              <span className="sr-only">Operately</span>
-            </a>
-          </div>
+    <header className="absolute inset-x-0 top-0 z-10">
+      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="/" className="-m-1.5 p-1.5">
+            <img className="h-6 w-auto" src={logoImage.src} alt="Operately logo" />
+            <span className="sr-only">Operately</span>
+          </a>
+        </div>
 
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <div className="hidden lg:flex items-center gap-x-6">
             {navigation.map((item) => (
               <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
                 {item.name}
               </a>
             ))}
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <div className="flex gap-x-6 items-center">
-              {/*
+            {/*
               <a href="/jobs"
                 className="bg-yellow-200 bg-opacity-50 px-2 py-1 rounded text-sm hover:underline">
                 We're hiring
               </a>
               */}
-              <a href={discordUrl} className=" hover:text-operately-blue">
-                <DiscordIcon className="h-6 w-6" />
-              </a>
-              <a href={githubUrl} className="hover:text-operately-blue">
-                <GitHubIcon className="h-6 w-6" />
-              </a>
-              <a href={xUrl} className="hover:text-operately-blue">
-                <XIcon className="h-6 w-6" />
-              </a>
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLSebV6j1nIvyjvyLptZ95mHXoj42XrnBmd5znVnUzU_6ATAJgw/viewform" className="bg-operately-blue hover:bg-operately-dark-blue text-white font-bold py-2 px-4 rounded">
-                Join the waitlist
-              </a>
-            </div>
-          </div>
-        </nav>
+            <a href={discordUrl} className=" hover:text-operately-blue">
+              <DiscordIcon className="h-6 w-6" />
+            </a>
+            <a href={githubUrl} className="hover:text-operately-blue">
+              <GitHubIcon className="h-6 w-6" />
+            </a>
+            <a href={xUrl} className="hover:text-operately-blue">
+              <XIcon className="h-6 w-6" />
+            </a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSebV6j1nIvyjvyLptZ95mHXoj42XrnBmd5znVnUzU_6ATAJgw/viewform" className="bg-operately-blue hover:bg-operately-dark-blue text-white font-bold py-2 px-4 rounded">
+              Join the cloud waitlist
+            </a>
+          </div >
+        </div >
+      </nav >
 
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
@@ -103,12 +101,12 @@ export default function Header() {
                     {item.name}
                   </a>
                 ))}
-                <a
-                    href="/jobs"
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    We're hiring
-                  </a>
+                {/*<a
+                  href="/jobs"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  We're hiring
+                </a>*/}
               </div>
               <div className="py-6">
                 <a
@@ -122,6 +120,6 @@ export default function Header() {
           </div>
         </Dialog.Panel>
       </Dialog>
-      </header>
+    </header >
   )
 }
