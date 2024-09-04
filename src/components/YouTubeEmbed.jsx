cc823b28d7d475f6f7cdf3536ab5c2483ddb1ae3
Igor Šarčević <igor@operately.com>
2024-09-04 10:25:00 +0000

    Add release to video
    Signed-off-by: Igor Šarčević <igor@operately.com>

--- src/components/YouTubeEmbed.jsx
+++ src/components/YouTubeEmbed.jsx
@@ -1,11 +1,17 @@
-export const YouTubeEmbed = ({ id }) => (
-  <iframe
+export const YouTubeEmbed = ({ id, showControls }) => {
+  let source = `https://www.youtube.com/embed/${id}`;
+
+  if (!showControls) {
+    source += `?controls=0`;
+  }
+
+  return <iframe
     className="w-full aspect-video"
-    src={`https://www.youtube.com/embed/${id}?controls=0`}
+    src={source}
     title="YouTube video player"
     frameBorder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     referrerPolicy="strict-origin-when-cross-origin"
     allowFullScreen
   ></iframe>
-);
+};
