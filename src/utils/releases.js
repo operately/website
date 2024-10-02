3fa6e0fb6e682d6aeb0f6be3a837183ae5ccfdaf
Marko Anastasov <marko@operately.com>
2024-10-02 12:17:59 +0200

    Return accidentally removed releases fetching function
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- src/utils/releases.js
+++ src/utils/releases.js
@@ -15,3 +15,14 @@ export async function getLatestRelease() {
     date: latestRelease.data.date.toISOString()
   };
 }
+
+export async function getPublishedReleases() {
+  const allReleases = await getCollection('releases');
+  
+  // If we're not in dev mode, we're gonna skip non-published releases
+  const isProduction = !import.meta.env.DEV;
+
+  return allReleases.filter(release => 
+    isProduction ? release.data.published !== false : true
+  ).sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
+}
