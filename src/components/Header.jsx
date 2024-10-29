a8fc503b60b66fc3812440a0e724a6acce383654
Marko Anastasov <marko@operately.com>
2024-10-29 12:28:09 +0100

    New navigation
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import logoImage from '../layouts/operately-logo.svg'
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import logoImage from "../layouts/operately-logo.svg";
 
-import { DiscordIcon, GitHubIcon, XIcon } from './Icons'
+import { DiscordIcon, GitHubIcon, XIcon } from "./Icons";
 
 const discordUrl = import.meta.env.DISCORD_URL;
 const githubUrl = import.meta.env.GITHUB_URL;
 const xUrl = import.meta.env.X_URL;
 
 const navigation = [
-  { name: "Releases", href: "/releases" }
-  /*{ name: 'Features', href: '#' },
-  { name: 'Company', href: '#' }*/
-]
+  { name: "Features", href: "/features" },
+  { name: "Releases", href: "/releases" },
+  { name: "Masterplan", href: "/masterplan" },
+];
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header className="absolute inset-x-0 top-0 z-10">
-      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav
+        className="flex items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
-            <img className="h-6 w-auto" src={logoImage.src} alt="Operately logo" />
+            <img
+              className="h-6 w-auto"
+              src={logoImage.src}
+              alt="Operately logo"
+            />
             <span className="sr-only">Operately</span>
           </a>
         </div>
@@ -39,36 +46,60 @@ export default function Header() {
           </button>
         </div>
 
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <div className="hidden lg:flex items-center gap-x-6">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900 hover:text-operately-blue">
-                {item.name}
-              </a>
-            ))}
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-sm font-semibold text-gray-900 hover:text-operately-blue"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:items-center space-x-6">
+          <div className="flex items-center space-x-4 mr-1">
             {/*
               <a href="/jobs"
-                className="bg-yellow-200 bg-opacity-50 px-2 py-1 rounded text-sm hover:underline">
+                className="bg-yellow-200 bg-opacity-50 px-2 py-1 rounded text-sm hover:underline flex items-center">
                 We're hiring
               </a>
               */}
-            <a href={discordUrl} className=" hover:text-operately-blue">
+            <a
+              href={discordUrl}
+              className=" hover:text-operately-blue flex items-center"
+            >
               <DiscordIcon className="h-6 w-6" />
             </a>
-            <a href={githubUrl} className="hover:text-operately-blue">
+            <a
+              href={githubUrl}
+              className="hover:text-operately-blue flex items-center"
+            >
               <GitHubIcon className="h-6 w-6" />
             </a>
-            <a href={xUrl} className="hover:text-operately-blue">
+            <a
+              href={xUrl}
+              className="hover:text-operately-blue flex items-center"
+            >
               <XIcon className="h-6 w-6" />
             </a>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSebV6j1nIvyjvyLptZ95mHXoj42XrnBmd5znVnUzU_6ATAJgw/viewform" className="bg-operately-blue hover:bg-operately-dark-blue text-white font-bold py-2 px-4 rounded">
-              Join the cloud waitlist
-            </a>
-          </div >
-        </div >
-      </nav >
+          </div>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSebV6j1nIvyjvyLptZ95mHXoj42XrnBmd5znVnUzU_6ATAJgw/viewform"
+            className="bg-operately-blue hover:bg-operately-dark-blue text-white font-bold py-2 px-4 rounded inline-flex items-center"
+          >
+            Join the cloud waitlist
+          </a>
+        </div>
+      </nav>
 
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
@@ -120,6 +151,6 @@ export default function Header() {
           </div>
         </Dialog.Panel>
       </Dialog>
-    </header >
-  )
+    </header>
+  );
 }
